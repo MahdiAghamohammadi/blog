f30dbe2a0c139abb21cded336f9a33e39c99fb61
mahdi <aghamohammadi.mehdi15@gmail.com>
2021-03-04 23:24:31 +0330

    work with Auth facade

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,12 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
+
+Route::get('/', function() {
     return view('welcome');
-});*/
-
-
-// Route::get('/', 'HomeController@index');
+});
 
 
 Route::resource('post', 'PostController');
