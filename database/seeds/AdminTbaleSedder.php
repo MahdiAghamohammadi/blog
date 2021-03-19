fb85481ca947c219dff8b89e8ac826cf1fabcaec
mahdi <aghamohammadi.mehdi15@gmail.com>
2021-03-19 13:11:28 +0330

    make seeder for admin table

---
+++ database/seeds/AdminTbaleSedder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminTbaleSedder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::create([
+            'name' => 'mahdi',
+            'email' => 'mahdi@gmail.com',
+            'password' => Hash::make(12345678),
+        ]);
+    }
+}
